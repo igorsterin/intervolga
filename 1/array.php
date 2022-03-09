2b9afd8d6467ec529f01e489cc7fd01b5605ba85
игорь стерин <igor234xb@gmail.com>
2022-03-09 23:43:43 +0700

    из первого задания убраны нативные php функции array_*

--- 1/array.php
+++ 1/array.php
@@ -1,14 +1,14 @@
 <?php
 
 
-$arr = [11,12,13,23,56,77,88,92,106];
+$arr = [11, 12, 13, 23, 56, 77, 88, 92, 106, 121, 133];
 $a = 'test';
 var_dump($arr);
 
 $pos = 0;
 foreach ($arr as $key => $item) {
     if (isNumberContainDigit($item, 2)) {
-        arrayInsert($arr, $a, $pos+1);
+        arrayInsert($arr, $a, $pos + 1);
         $pos++;
     }
     $pos++;
@@ -20,14 +20,23 @@ var_dump($arr);
 
 function arrayInsert(&$arr, $elem, $position)
 {
-   $arr = array_merge(array_slice($arr, 0, $position), [$elem], array_slice($arr, $position, count($arr)));
+    $countArray = count($arr);
+
+    if ($position > $countArray) {
+        $position = $countArray;
+    }
+    for ($i = $countArray; $i >= $position; $i--) {
+        $arr[$i] = $arr[$i - 1];
+    }
+    $arr[$position] = $elem;
 }
 
 function isNumberContainDigit($number = '', $digit = ''): bool
 {
-    if (!$digit) return false;
+    if (!$digit) {
+        return false;
+    }
     $strNum = strval($number);
     $strDigit = strval($digit);
     return !(strpos($strNum, $strDigit) === false);
-
 }
